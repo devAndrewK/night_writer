37bd13c50f0069fea59cb6d701c3ef996dfcf9ed
Andrew King <dev.andrewking@gmail.com>
2022-04-26 18:44:56 -0500

    refactor night_writer runner file to utilize new method with accompanying text files

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,3 @@
 require './lib/file_reader'
 file_reader = FileReader.new
-#file_reader.read
-file_reader.convert_to_braille
-file_reader.write_braille
-file_reader.print_output_message
-
-require 'pry';binding.pry
+file_reader.english_to_braille_routine
